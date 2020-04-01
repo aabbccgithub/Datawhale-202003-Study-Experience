390d7c882d4c178b807bc703fd8afce6433615f3
FU <33396226+aabbccgithub@users.noreply.github.com>
2020-04-01 21:26:07 +0800

    add task4
    task 4 code by python

---
+++ task4.py
@@ -0,0 +1,107 @@
+import pandas as pd
+import numpy as np
+ 
+def reduce_mem_usage(df):
+    """ iterate through all the columns of a dataframe and modify the data type
+        to reduce memory usage.        
+    """
+    start_mem = df.memory_usage().sum() / 1024**2
+    print('Memory usage of dataframe is {:.2f} MB'.format(start_mem))
+    
+    for col in df.columns:
+        col_type = df[col].dtype
+        
+        if col_type != object:
+            c_min = df[col].min()
+            c_max = df[col].max()
+            if str(col_type)[:3] == 'int':
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)  
+            else:
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)
+        else:
+            df[col] = df[col].astype('category')
+ 
+    end_mem = df.memory_usage().sum() / 1024**2
+    print('Memory usage after optimization is: {:.2f} MB'.format(end_mem))
+    print('Decreased by {:.1f}%'.format(100 * (start_mem - end_mem) / start_mem))
+    
+    return df
+def import_data(file,encoding):
+    """create a dataframe and optimize its memory usage"""
+    df = pd.read_csv(file, parse_dates=True, keep_date_col=True,encoding=encoding)
+    df = reduce_mem_usage(df)
+    return df
+
+
+sample_feature = sample_feature.dropna().replace('-', 0).reset_index(drop=True)
+sample_feature['notRepairedDamage'] = sample_feature['notRepairedDamage'].astype(np.float32)
+train = sample_feature[continuous_feature_names + ['price']]
+
+train_X = train[continuous_feature_names]
+train_y = train['price']
+from sklearn.linear_model import LinearRegression
+model = LinearRegression(normalize=True)
+model = model.fit(train_X, train_y)
+
+#可视化
+from matplotlib import pyplot as plt
+subsample_index = np.random.randint(low=0, high=len(train_y), size=50)
+plt.scatter(train_X['v_9'][subsample_index], train_y[subsample_index], color='black')
+plt.scatter(train_X['v_9'][subsample_index], model.predict(train_X.loc[subsample_index]), color='blue')
+plt.xlabel('v_9')
+plt.ylabel('price')
+plt.legend(['True Price','Predicted Price'],loc='upper right')
+print('The predicted price is obvious different from true price')
+plt.show()
+
+#线性模型
+result = pd.DataFrame(result)
+result.index = ['cv' + str(x) for x in range(1, 6)]
+result
+model = LinearRegression().fit(train_X, train_y_ln)
+print('intercept:'+ str(model.intercept_))
+sns.barplot(abs(model.coef_), continuous_feature_names)
+model = Ridge().fit(train_X, train_y_ln)
+print('intercept:'+ str(model.intercept_))
+sns.barplot(abs(model.coef_), continuous_feature_names)
+#
+model = Lasso().fit(train_X, train_y_ln)
+print('intercept:'+ str(model.intercept_))
+sns.barplot(abs(model.coef_), continuous_feature_names)
+
+#常用非线性模型
+from sklearn.linear_model import LinearRegression
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.neural_network import MLPRegressor
+from xgboost.sklearn import XGBRegressor
+from lightgbm.sklearn import LGBMRegressor
+models = [LinearRegression(),
+          DecisionTreeRegressor(),
+          RandomForestRegressor(),
+          GradientBoostingRegressor(),
+          MLPRegressor(solver='lbfgs', max_iter=100), 
+          XGBRegressor(n_estimators = 100, objective='reg:squarederror'), 
+          LGBMRegressor(n_estimators = 100)]
+          result = dict()
+for model in models:
+    model_name = str(model).split('(')[0]
+    scores = cross_val_score(model, X=train_X, y=train_y_ln, verbose=0, cv = 5, scoring=make_scorer(mean_absolute_error))
+    result[model_name] = scores
+    print(model_name + ' is finished')
+    result = pd.DataFrame(result)
+result.index = ['cv' + str(x) for x in range(1, 6)]
